d18886b06c71cf86081bbec323ecb150c239229c
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2022-01-24 12:32:07 +0600

    Add files via upload

---
+++ cli/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+const Footer = () => (
+
+  <h4 style={{textAlign:'center',marginTop:'10px'}} className="text-white ">© DZ. Web3 Blockchain.</h4>
+  
+);
+
+export default Footer;
